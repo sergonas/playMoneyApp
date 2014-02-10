4c8929cd84bbd3b2f819cf65853217e547b63127
Zalizko Sergey <sergonas@gmail.com>
2014-02-10 23:24:03 +0400

    Небольшой рефакторинг

--- app/controllers/Money.scala
+++ app/controllers/Money.scala
@@ -2,7 +2,11 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import anorm._
 import models._
+import views._
 
 object Money extends Controller {
   def index = Action {
